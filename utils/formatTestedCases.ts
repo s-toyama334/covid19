620d6b313242e0751d988a3aec9b31dc7352bb0e
momujin <juumonji@daily-tohoku.co.jp>
2020-04-03 22:33:27 +0900

    Update formatTestedCases.ts

--- utils/formatTestedCases.ts
+++ utils/formatTestedCases.ts
@@ -7,11 +7,11 @@ type DataType = {
       value: number
       children: [
         {
-          attr: '都内発生件数'
+          attr: '陰性件数'
           value: number
         },
         {
-          attr: 'その他件数'
+          attr: '陽性件数'
           value: number
         }
       ]
@@ -22,8 +22,8 @@ type DataType = {
 type TestedCasesType = {
   累計人数: number
   合計件数: number
-  都内発生件数: number
-  その他件数: number
+  陰性件数: number
+  陽性件数: number
 }
 
 /**
@@ -35,8 +35,8 @@ export default (data: DataType) => {
   const formattedData: TestedCasesType = {
     累計人数: data.value,
     合計件数: data.children[0].value,
-    都内発生件数: data.children[0].children[0].value,
-    その他件数: data.children[0].children[1].value
+    陰性件数: data.children[0].children[0].value,
+    陽性件数: data.children[0].children[1].value
   }
   return formattedData
 }
