ed2c13aa0feca28c18dda1400a9cf9d6698db94c
momujin <juumonji@daily-tohoku.co.jp>
2020-06-18 10:36:55 +0900

    test

--- components/cards/ConfirmedCasesDetailsCard.vue
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -10,9 +10,6 @@
         v-bind="confirmedCases"
       />
     </confirmed-cases-card>
-    <template v-slot:description>
-      {{ $t('※土・日・祝日を除く庁舎開庁日の1週間累計数') }}
-    </template>
   </v-col>
 </template>
 
