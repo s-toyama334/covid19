794c3fd288f8d16ccaa1df13716f948cb8e167a3
momujin <juumonji@daily-tohoku.co.jp>
2020-06-18 11:15:53 +0900

    test

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -9,7 +9,6 @@
       :unit="$t('人')"
       :url="'https://catalog.data.metro.tokyo.lg.jp/dataset/t000010d0000000068'"
     />
-    <p class="annotation" style="font-size:12px; margin-top:10px;">※米軍三沢基地内の感染者は含まれていない</p>
   </v-col>
 </template>
 
